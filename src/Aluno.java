99f958e670b315c3d7f38f9efcb1822ad45ab953
kaue-andrade <kauedd22@gmail.com>
2022-05-22 15:13:02 -0300

    exercicio3

---
+++ src/Aluno.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Aluno {
+    public static void main(String[] agrs){
+
+        System.out.println("\n=-=-=Sistema de cadastramento de alunos do IFBA=-=-=");
+
+        String nome;
+        String cpf;
+        String dataNascimento;
+        String validador;
+
+        Scanner ler = new Scanner(System.in);
+
+        ArrayList<Integer> Aluno = new ArrayList<>();
+
+        while (validador != "-1"){
+            System.out.println("A");
+
+        }
+    }
+}
--- src/Precos.java
+++ src/Precos.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class questao1 {
+public class Precos {
     public static void main(String[] args) {
 
         int contador = 0;
